b80fc9d17043aef3be1cac380ada2381c5c1c8b1
achyutharamareddy-debug <achyutharamareddy@gmail.com>
2025-11-11 14:56:51 +0530

    Add files via upload

---
+++ trace.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() 
+{
+    int a[100][100],sum=0,i,j,n;
+
+    printf("\n enter array size: ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("\n enter value at a[%d][%d]: ",i,j);
+            scanf("%d",&a[i][j]);}
+    }
+    for(i=0;i<n;i++)
+    {
+    	sum=sum+a[i][i];}
+    	printf("\n sum=%d",sum);
+    return 0;
+}
