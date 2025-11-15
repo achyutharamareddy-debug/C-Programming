3c6da5901fb73564d9d18fe54a573cb171bad9e1
achyutharamareddy-debug <achyutharamareddy@gmail.com>
2025-11-15 11:13:19 +0530

    Add files via upload

---
+++ strcmp1.cpp
@@ -0,0 +1,11 @@
+# include<stdio.h>
+# include<string.h>
+int main()
+{
+	char a[200]="ADITYA";
+	char b[200]="ADITYA";
+	int result1;
+	result1=strcmp(a,b);
+	printf("\n %d",result1);
+	return 0;
+}
---
+++ strlwr1.cpp
@@ -0,0 +1,11 @@
+# include<stdio.h>
+# include<string.h>
+int main()
+{
+	char a[200];
+	printf("\n enter any string");
+	gets(a);
+	strlwr(a);
+	puts(a);
+	return 0;
+}
